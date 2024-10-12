145fbe60c32580bc00f4de6eb5d37faac88b154b
Intummadee <kataroja1@gmail.com>
2024-10-12 19:37:57 +0700

    before install agentpy

--- app/Hello.py
+++ app/Hello.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/") # การสร้าง path route จะเรียกใช้งานฟังชันด้านล่างเลย
 def index():
-    return "<h1>Hello, World!</h1>"
+   return render_template("index.html")
+
+@app.route("/about") # การสร้าง path route จะเรียกใช้งานฟังชันด้านล่างเลย
+def about():
+   name = "Nahida"
+   return render_template("about.html", myMain = name)
 
 @app.route("/user/<name>/<age>") 
 def member(name,age):
-    return "<h1>Hello, World! {} , age: {} </h1>".format(name,age)
+   return "<h1>Hello, World! {} , age: {} </h1>".format(name,age)
 
 
 if __name__ == "__main__":
